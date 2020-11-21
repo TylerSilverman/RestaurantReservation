d172890984346b8346c96c1114a0e029e4c4268b
nbmc1991 <nbmc1991@gmail.com>
2020-11-21 12:32:33 -0500

    frontend js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { table } = require("console");
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -7,15 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const tableReservations = [
-        {
-          routeName: "yoda",
-          name: "Yoda",
-          role: "Jedi Master",
-          age: 900,
-          forcePoints: 2000
-        },
-    ]
+const tableReservations = [];
 
 const waitingList = [];
 
@@ -37,16 +30,18 @@ app.get("/api/tables", function(req, res){
 
 });
 
-app.post("/api/tables", function(req, res){
-    
-})
-
 
 app.post("/api/reserve", function (req, res){
 var newReservation = req.body;
-newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
-
 console.log(newReservation);
+if(tableReservations.length<5){
+    tableReservations.push(newReservation)
+    return res.json({reserved:true})
+}else{
+    waitingList.push(newReservation)
+    return res.json({reserved:false})
+};
+
 });
 
 
